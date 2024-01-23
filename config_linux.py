c6d16c7ba739df501b33496c18310e6fbf5d7344
Zbrooklyn <edwardzbrooklyn+forum@gmail.com>
2024-01-23 14:30:16 -0500

    update code

---
+++ config_linux.py
@@ -0,0 +1,10 @@
+import os
+
+# Base path for the base speakers' models and configurations
+BASE_SPEAKERS_PATH = os.path.join('/home/zbrooklyn/Downloads/checkpoints', 'base_speakers', 'EN')
+
+# Base path for the converter models and configurations
+CONVERTER_PATH = os.path.join('/home/zbrooklyn/Downloads/checkpoints', 'converter')
+
+# Output directory path
+OUTPUTS_PATH = '/home/zbrooklyn/Documents/GitHub/Local-Low-Latency-Speech-to-Speech/outputs'
